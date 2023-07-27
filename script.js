e1983792cfc9ad4f31f0d691880d83f3d8251dda
ParasPandey019 <paraspandey019@gmail.com>
2023-07-27 23:16:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function buttonClick(id){
+    var disp = document.getElementById("display");
+    var elem = document.getElementById(id);
+    var value = elem.innerHTML;
+    disp.innerHTML = disp.innerHTML + value;
+}
+
+function chooseSign(){
+    
+}
+
+function clearButton(){
+    var disp = document.getElementById("display");
+    disp.innerHTML = "";
+}
+
+function evaluate(){
+    var disp = document.getElementById("display");
+    
+}
+
